cddd0ca4de62c85e6d1d37e973bce2fc7b45eed3
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2022-06-06 17:01:43 -0300

    Classes Ghost and Player updated -- attributes, action methods, constructors and gets and sets

--- src/br/com/fiap/pacman/Ghost.java
+++ src/br/com/fiap/pacman/Ghost.java
@@ -1,5 +1,59 @@
 package br.com.fiap.pacman;
 
-public class Ghost extends GameObject{
+public class Ghost extends GameObject {
+
+	// ATTRIBUTES
+	private int direction;
+
+	// ACTION METHODS
+	public boolean canMove() {
+		int newY = getY();
+		int newX = getX();
+
+		if (direction == 0)
+			newY = getY() - 10;
+		else if (direction == 90)
+			newX = getX() + 10;
+		else if (direction == 180)
+			newY = getY() + 10;
+		else if (direction == 270)
+			newX = getX() - 10;
+
+		if (newY < 0 | newY > getScreenSize())
+			return false;
+		else if (newX < 0 | newX > getScreenSize())
+			return false;
+		else
+			return true;
+	}
+
+	public void move() {
+		if (direction == 0)
+			setX(getX() - 10);
+		else if (direction == 90)
+			setY(getY() + 10);
+		else if (direction == 180)
+			setX(getX() + 10);
+		else if (direction == 270)
+			setY(getY() - 10);
+	}
+
+	// CONSTRUCTORS
+	public Ghost(int x, int y, int direction) {
+		super(x, y);
+		this.direction = direction;
+	}
+
+	public Ghost() {
+	}
+
+	// GETS AND SETS
+	public int getDirection() {
+		return direction;
+	}
+
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
 
 }
--- src/br/com/fiap/pacman/Player.java
+++ src/br/com/fiap/pacman/Player.java
@@ -1,5 +1,85 @@
 package br.com.fiap.pacman;
 
-public class Player extends GameObject{
+public class Player extends GameObject {
 
+	// ATTRIBUTES
+	private int direction;
+	private int life;
+	private boolean invincible;
+	private int invincibleTime;
+
+	// ACTION METHODS
+	public boolean canMove() {
+		int newY = getY();
+		int newX = getX();
+
+		if (direction == 0)
+			newY = getY() - 10;
+		else if (direction == 90)
+			newX = getX() + 10;
+		else if (direction == 180)
+			newY = getY() + 10;
+		else if (direction == 270)
+			newX = getX() - 10;
+
+		if (newY < 0 | newY > getScreenSize())
+			return false;
+		else if (newX < 0 | newX > getScreenSize())
+			return false;
+		else
+			return true;
+	}
+
+	public void move() {
+		if (direction == 0)
+			setX(getX() - 10);
+		else if (direction == 90)
+			setY(getY() + 10);
+		else if (direction == 180)
+			setX(getX() + 10);
+		else if (direction == 270)
+			setY(getY() - 10);
+	}
+
+	// CONSTRUCTORS
+	public Player(int x, int y, int direction) {
+		super(x, y);
+		this.direction = direction;
+	}
+
+	public Player() {
+	}
+
+	// GETS AND SETS
+	public int getDirection() {
+		return direction;
+	}
+
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public void setLife(int life) {
+		this.life = life;
+	}
+
+	public boolean isInvincible() {
+		return invincible;
+	}
+
+	public void setInvincible(boolean invincible) {
+		this.invincible = invincible;
+	}
+
+	public int getInvincibleTime() {
+		return invincibleTime;
+	}
+
+	public void setInvincibleTime(int invincibleTime) {
+		this.invincibleTime = invincibleTime;
+	}
 }
